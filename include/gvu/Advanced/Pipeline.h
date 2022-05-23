7baa878808de7eb683a04fbd30888e3047545ead
Gavin <gavin.n.lobo@gmail.com>
2022-05-23 17:41:00 -0400

    Added Pipeline and VulkanApplicationContext

---
+++ include/gvu/Advanced/Pipeline.h
@@ -0,0 +1,252 @@
+#ifndef GVU_VULKAN_APPLICATION_CONTEXT_PIPELINE_H
+#define GVU_VULKAN_APPLICATION_CONTEXT_PIPELINE_H
+
+#include <iostream>
+#include <cassert>
+#include <gvu/Extension/spirvPipelineReflector.h>
+#include <gvu/Core/GraphicsPipelineCreateInfo.h>
+#include <GLSLCompiler.h>
+
+struct VulkanApplicationContext;
+
+struct PipelineBase
+{
+    VkPipeline getPipeline() const
+    {
+        return pipeline;
+    }
+    gvu::spirvPipelineReflector const & getReflector() const
+    {
+        return reflector;
+    }
+    VkDescriptorSetLayout getDescriptorSetLayout(uint32_t setNumber) const
+    {
+        return setLayouts.at(setNumber);
+    }
+    /**
+     * @brief allocateDescriptorSet
+     * @param setNumber
+     * @return
+     *
+     * Allocate a descriptor set for a particular set layout
+     * within this pipeline
+     */
+    VkDescriptorSet allocateDescriptorSet(uint32_t setNumber) const;
+protected:
+    VkShaderModule _createShader(std::vector<uint32_t> code);
+
+    VkPipeline                                          pipeline = VK_NULL_HANDLE;
+    std::unordered_map<uint32_t, VkDescriptorSetLayout> setLayouts;
+    gvu::spirvPipelineReflector                         reflector;
+    std::shared_ptr<VulkanApplicationContext>           context;
+
+    friend struct VulkanApplicationContext;
+};
+
+
+struct Pipeline;
+
+struct ShaderStage
+{
+    VkShaderStageFlagBits                        stage;
+    std::string                                  glslCode;
+    gnl::GLSLCompiler                            compiler;
+    std::vector<uint32_t>                        spirvCode;
+
+    /**
+     * @brief loadVertexGLSL
+     * @param p
+     * @param includeFilePathAsIncludeDir
+     * @return
+     *
+     * Load GLSL code from a file path.
+     */
+    ShaderStage& loadGLSL(std::filesystem::path const & p, bool includeFilePathAsIncludeDir=true)
+    {
+        std::ifstream t(p);
+        std::string srcString((std::istreambuf_iterator<char>(t)),
+                         std::istreambuf_iterator<char>());
+
+        glslCode = std::move(srcString);
+        if(includeFilePathAsIncludeDir)
+        {
+            compiler.addIncludePath( p.parent_path() );
+        }
+        return *this;
+    }
+    ShaderStage& setGLSL(std::string const & srcString)
+    {
+        glslCode = std::move(srcString);
+        return *this;
+    }
+
+
+    ShaderStage& appendIncludePath(std::filesystem::path const & p)
+    {
+        compiler.addIncludePath(p.native());
+        return *this;
+    }
+
+
+    ShaderStage& addCompileTimeDefinition(std::string const & var, std::string const &value)
+    {
+        compiler.addCompleTimeDefinition(var, value);
+        return *this;
+    }
+
+    void compile()
+    {
+        EShLanguage _type;
+        switch(stage)
+        {
+            case VK_SHADER_STAGE_VERTEX_BIT: _type = EShLangVertex; break;
+            case VK_SHADER_STAGE_TESSELLATION_CONTROL_BIT:  _type = EShLangTessControl; break;
+            case VK_SHADER_STAGE_TESSELLATION_EVALUATION_BIT:  _type = EShLangTessEvaluation; break;
+            case VK_SHADER_STAGE_GEOMETRY_BIT:  _type = EShLangGeometry; break;
+            case VK_SHADER_STAGE_FRAGMENT_BIT:  _type = EShLangFragment; break;
+            case VK_SHADER_STAGE_COMPUTE_BIT:  _type = EShLangCompute; break;
+            case VK_SHADER_STAGE_RAYGEN_BIT_KHR:  _type = EShLangRayGen; break;
+            case VK_SHADER_STAGE_ANY_HIT_BIT_KHR:  _type = EShLangAnyHit; break;
+            case VK_SHADER_STAGE_CLOSEST_HIT_BIT_KHR:  _type = EShLangClosestHit; break;
+            case VK_SHADER_STAGE_MISS_BIT_KHR:  _type = EShLangMiss; break;
+            case VK_SHADER_STAGE_INTERSECTION_BIT_KHR:  _type = EShLangIntersect; break;
+            case VK_SHADER_STAGE_CALLABLE_BIT_KHR:  _type = EShLangCallable; break;
+            case VK_SHADER_STAGE_TASK_BIT_NV:  _type = EShLangTaskNV; break;
+            case VK_SHADER_STAGE_MESH_BIT_NV: _type = EShLangMeshNV; break;
+            case VK_SHADER_STAGE_SUBPASS_SHADING_BIT_HUAWEI:
+            default:
+    #if 0
+            case VK_SHADER_STAGE_ALL_GRAPHICS:
+            case VK_SHADER_STAGE_ALL:
+            case VK_SHADER_STAGE_RAYGEN_BIT_NV:
+            case VK_SHADER_STAGE_ANY_HIT_BIT_NV:  _type = EShLangAnyHitNV; break;
+            case VK_SHADER_STAGE_CLOSEST_HIT_BIT_NV:  _type = EShLangClosestHitNV; break;
+            case VK_SHADER_STAGE_MISS_BIT_NV:  _type = EShLangMissNV; break;
+            case VK_SHADER_STAGE_INTERSECTION_BIT_NV:  _type = EShLangIntersectNV; break;
+            case VK_SHADER_STAGE_CALLABLE_BIT_NV:  _type = EShLangCallableNV; break;
+            case VK_SHADER_STAGE_FLAG_BITS_MAX_ENUM:  _type = EShLangVertex; break;
+    #endif
+                break;
+        }
+
+        spirvCode = compiler.compile(glslCode, _type);
+    }
+
+
+    std::shared_ptr<VulkanApplicationContext> context;
+      friend struct VulkanApplicationContext;
+};
+
+
+struct ComputePipeline : public PipelineBase
+{
+    VkPipelineLayout getPipelineLayout() const
+    {
+        return createInfo.layout;
+    }
+
+    /**
+     * @brief build
+     *
+     * Build the pipeline
+     */
+    void build();
+
+    /**
+     * @brief destroy
+     *
+     * Destroy the pipeline
+     */
+    void destroy();
+
+
+    ShaderStage & getComputeStage()
+    {
+        return computeStage;
+    }
+protected:
+    VkComputePipelineCreateInfo createInfo = {VK_STRUCTURE_TYPE_COMPUTE_PIPELINE_CREATE_INFO};
+    ShaderStage computeStage = {VK_SHADER_STAGE_COMPUTE_BIT};
+    friend class VulkanApplicationContext;
+};
+
+
+/**
+ * @brief The Pipeline struct
+ *
+ * This pipeline struct is a help object and wrapper around
+ * a VkPipeline.
+ */
+struct Pipeline : public PipelineBase
+{
+
+public:
+    VkPipelineLayout getPipelineLayout() const
+    {
+        return createInfo.pipelineLayout;
+    }
+    VkRenderPass getRenderPass() const
+    {
+        return createInfo.renderPass;
+    }
+
+    void setRenderPass(VkRenderPass rp)
+    {
+        createInfo.renderPass = rp;
+    }
+
+    /**
+     * @brief build
+     *
+     * Build the pipeline
+     */
+    void build();
+
+    /**
+     * @brief destroy
+     *
+     * Destroy the pipeline
+     */
+    void destroy();
+
+
+    ShaderStage& getVertexStage()
+    {
+        return vertexStage;
+    }
+
+    ShaderStage& getFragmentStage()
+    {
+        return fragmentStage;
+    }
+
+    /**
+     * @brief setOutputFormat
+     * @param index
+     * @param format
+     *
+     * Sets the output color format
+     */
+    void setOutputFormat(uint32_t index, VkFormat format)
+    {
+        createInfo.setOutputFormat(index, format);
+    }
+    void setDepthFormat(VkFormat format)
+    {
+        createInfo.setDepthFormat(format);
+    }
+    auto & getCreateInfo()
+    {
+        return createInfo;
+    }
+protected:
+    gvu::GraphicsPipelineCreateInfo createInfo;
+    ShaderStage vertexStage = {VK_SHADER_STAGE_VERTEX_BIT};
+    ShaderStage fragmentStage  = {VK_SHADER_STAGE_FRAGMENT_BIT};
+
+    friend struct VulkanApplicationContext;
+};
+
+
+
+#endif
---
+++ include/gvu/Advanced/VulkanApplicationContext.h
@@ -0,0 +1,269 @@
+#ifndef GVU_VULKAN_APPLICATION_CONTEXT_H
+#define GVU_VULKAN_APPLICATION_CONTEXT_H
+
+#include <iostream>
+#include <cassert>
+#include <gvu/Core/Managers/CommandPoolManager.h>
+#include <gvu/Core/Managers/DescriptorPoolManager.h>
+#include <gvu/Core/Cache/TextureCache.h>
+#include <gvu/Core/Cache/SamplerCache.h>
+#include <gvu/Core/Cache/DescriptorSetLayoutCache.h>
+#include <gvu/Core/Cache/RenderPassCache.h>
+#include <gvu/Core/Cache/PipelineLayoutCache.h>
+#include <gvu/Core/GraphicsPipelineCreateInfo.h>
+#include <gvu/Extension/spirvPipelineReflector.h>
+#include <GLSLCompiler.h>
+
+#include "Pipeline.h"
+
+struct VulkanApplicationContext;
+
+
+/**
+ * @brief The VulkanApplicationContext struct
+ *
+ * The VulkanApplicationContext is a struct which
+ * holds all the managers/caches which can be used
+ * to create vulkan objects.
+ *
+ */
+struct VulkanApplicationContext : std::enable_shared_from_this<VulkanApplicationContext>
+{
+    VmaAllocator                  allocator = nullptr;
+    gvu::MemoryCache              memoryCache;// allocate textures from here
+    gvu::RenderPassCache          renderPassCache;
+    gvu::SamplerCache             samplerCache;
+    gvu::DescriptorSetLayoutCache descriptorSetLayoutCache;
+    gvu::PipelineLayoutCache      pipelineLayoutCache;
+    gvu::DescriptorSetAllocator   descriptorSetAllocator;
+    gvu::CommandPoolManager       commandPool;
+    gvu::CommandPoolManager2      commandPoolManager;
+
+    void init(VkInstance instance,
+              VkPhysicalDevice physicalDevice,
+              VkDevice device,
+              VkQueue graphicsQueue)
+    {
+        VmaAllocatorCreateInfo outInfo = {};
+        outInfo.physicalDevice   = physicalDevice;
+        outInfo.device           = device;
+        outInfo.instance         = instance;
+        outInfo.vulkanApiVersion = VK_API_VERSION_1_3;
+        VkResult res = vmaCreateAllocator(&outInfo, &allocator);
+
+        memoryCache.init(physicalDevice, device, graphicsQueue, allocator);
+        renderPassCache.init(device);
+        samplerCache.init(device);
+        descriptorSetLayoutCache.init(device);
+        pipelineLayoutCache.init(device);
+        descriptorSetAllocator.init(&descriptorSetLayoutCache);
+        commandPool.init(device,physicalDevice,graphicsQueue);
+        commandPoolManager.init(device, physicalDevice, graphicsQueue);
+
+        for(int i=0;i<5;i++)
+            commandPoolManager.getCommandPool();
+    }
+
+    void destroy()
+    {
+        pipelineLayoutCache.destroy();
+        descriptorSetLayoutCache.destroy();
+        samplerCache.destroy();
+        renderPassCache.destroy();
+        memoryCache.destroy();
+        commandPool.destroy();
+        commandPoolManager.destroy();
+        descriptorSetAllocator.destroy();
+        vmaDestroyAllocator(allocator);
+    }
+
+    VkDevice getDevice() const
+    {
+        return memoryCache.getDevice();
+    }
+
+
+
+    /**
+     * @brief createBuffer
+     * @param bytes
+     * @param usage
+     * @param memUsage
+     * @return
+     *
+     * Create a buffer
+     */
+    std::shared_ptr<gvu::BufferInfo> createBuffer(size_t bytes, VkBufferUsageFlags usage, VmaMemoryUsage memUsage)
+    {
+        return memoryCache.allocateBuffer(bytes, usage, memUsage);
+    }
+
+
+    /**
+     * @brief makePipeline
+     * @return
+     *
+     * Returns a shared pipeline object with the appropriate
+     * pointers created.
+     */
+    std::shared_ptr<Pipeline> makePipeline()
+    {
+        std::shared_ptr<Pipeline> p = std::make_shared<Pipeline>();
+        p->context = shared_from_this();
+        p->vertexStage.context = p->context;
+        p->fragmentStage.context = p->context;
+        return p;
+    }
+
+    /**
+     * @brief makeComputePipeline
+     * @return
+     *
+     * Returns a shared pointer to a compute pipeline
+     */
+    std::shared_ptr<ComputePipeline> makeComputePipeline()
+    {
+        std::shared_ptr<ComputePipeline> p = std::make_shared<ComputePipeline>();
+        p->context = shared_from_this();
+        p->computeStage.context = p->context;
+        return p;
+    }
+
+    VkDescriptorSet allocateDescriptorSet(VkDescriptorSetLayout layout)
+    {
+        return descriptorSetAllocator.allocate(layout);
+    }
+
+    void releaseDescriptorSet(VkDescriptorSet set)
+    {
+        descriptorSetAllocator.releaseToPool(set);
+    }
+};
+
+inline void Pipeline::destroy()
+{
+    vkDestroyPipeline(context->getDevice(), pipeline, nullptr);
+}
+
+inline void Pipeline::build()
+{
+    {
+
+        createInfo.vertexShader = _createShader(vertexStage.spirvCode);
+        createInfo.fragmentShader = _createShader(fragmentStage.spirvCode);
+    }
+
+    reflector.addSPIRVCode(vertexStage.spirvCode, VK_SHADER_STAGE_VERTEX_BIT);
+    reflector.addSPIRVCode(fragmentStage.spirvCode, VK_SHADER_STAGE_FRAGMENT_BIT);
+
+    auto pipelineLayoutCreateInfo = reflector.generateCombinedPipelineLayoutCreateInfo();
+    createInfo.pipelineLayout = pipelineLayoutCreateInfo.create(context->pipelineLayoutCache, context->descriptorSetLayoutCache);
+
+    uint32_t set=0;
+    for(auto & c : pipelineLayoutCreateInfo.setLayoutInfos)
+    {
+        auto l = context->descriptorSetLayoutCache.create(c);
+        setLayouts[set] = l;
+    }
+
+
+    //createInfo.enableDepthTest = true;
+    //createInfo.enableDepthWrite = true;
+    createInfo.dynamicStates.push_back(VK_DYNAMIC_STATE_VIEWPORT);
+    createInfo.dynamicStates.push_back(VK_DYNAMIC_STATE_SCISSOR);
+    createInfo.dynamicStates.push_back(VK_DYNAMIC_STATE_CULL_MODE);
+    createInfo.dynamicStates.push_back(VK_DYNAMIC_STATE_FRONT_FACE);
+    createInfo.dynamicStates.push_back(VK_DYNAMIC_STATE_PRIMITIVE_TOPOLOGY);
+
+    //createInfo.setVertexInputs({
+    //                       VK_FORMAT_R32G32B32_SFLOAT, // position
+    //                       VK_FORMAT_R32G32B32_SFLOAT, // normal
+    //                       VK_FORMAT_R32G32B32A32_SFLOAT, // tangent
+    //
+    //                       VK_FORMAT_R32G32_SFLOAT, // tex0
+    //                       VK_FORMAT_R32G32_SFLOAT, // tex1
+    //
+    //                       VK_FORMAT_R8G8B8A8_UNORM, // color 0
+    //                       VK_FORMAT_R16G16B16A16_UINT,
+    //                       VK_FORMAT_R32G32B32A32_SFLOAT,
+    //                   });
+
+
+    pipeline = createInfo.create([&](auto & C)
+    {
+        VkPipeline p2 = VK_NULL_HANDLE;
+        auto result = vkCreateGraphicsPipelines(context->getDevice(), nullptr, 1, &C, nullptr, &p2);
+        if(result != VK_SUCCESS)
+        {
+
+            //throw std::runtime_error("Failed at compiling pipeline");
+        }
+        return p2;
+    });
+
+
+    vkDestroyShaderModule(context->getDevice(), createInfo.vertexShader  , nullptr);
+    vkDestroyShaderModule(context->getDevice(), createInfo.fragmentShader, nullptr);
+
+    createInfo.vertexShader = VK_NULL_HANDLE;
+    createInfo.fragmentShader = VK_NULL_HANDLE;
+}
+
+inline void ComputePipeline::destroy()
+{
+    vkDestroyPipeline(context->getDevice(), pipeline, nullptr);
+}
+
+inline void ComputePipeline::build()
+{
+
+    {
+        createInfo.stage.sType  = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+        createInfo.stage.module = _createShader(computeStage.spirvCode);
+        createInfo.stage.pName  = "main";
+        createInfo.stage.stage  = VK_SHADER_STAGE_COMPUTE_BIT;
+    }
+    reflector.addSPIRVCode(computeStage.spirvCode, VK_SHADER_STAGE_COMPUTE_BIT);
+
+    auto pipelineLayoutCreateInfo = reflector.generateCombinedPipelineLayoutCreateInfo();
+    createInfo.layout = pipelineLayoutCreateInfo.create(context->pipelineLayoutCache, context->descriptorSetLayoutCache);
+
+    uint32_t set=0;
+    for(auto & c : pipelineLayoutCreateInfo.setLayoutInfos)
+    {
+        auto l = context->descriptorSetLayoutCache.create(c);
+        setLayouts[set] = l;
+    }
+
+    auto result = vkCreateComputePipelines(context->getDevice(), nullptr, 1, &createInfo, nullptr, &pipeline);
+    if(result != VK_SUCCESS)
+    {
+        throw std::runtime_error("Failed at compiling pipeline");
+    }
+
+    vkDestroyShaderModule(context->getDevice(), createInfo.stage.module, nullptr);
+    createInfo.stage.module = VK_NULL_HANDLE;
+}
+
+inline VkDescriptorSet PipelineBase::allocateDescriptorSet(uint32_t setNumber) const
+{
+    return context->allocateDescriptorSet(getDescriptorSetLayout(setNumber));
+}
+
+inline VkShaderModule PipelineBase::_createShader(std::vector<uint32_t> code)
+{
+    VkShaderModuleCreateInfo ci = {};
+    ci.sType    = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO;
+    ci.codeSize = static_cast<uint32_t>(code.size() * 4);
+    ci.pCode    = code.data();
+
+    VkShaderModule sh = {};
+    auto result = vkCreateShaderModule(context->getDevice(), &ci, nullptr, &sh);
+    if(result != VK_SUCCESS)
+    {
+        throw std::runtime_error("Failed at compiling shader");
+    }
+    return sh;
+}
+
+#endif
