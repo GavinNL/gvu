78b02b9ccba886938dec2057dc61f58ca5718190
Gavin <gavin.n.lobo@gmail.com>
2022-10-11 15:18:33 -0400

    Added subbuffermanager

---
+++ include/gvu/Core/Managers/SubBufferManager.h
@@ -0,0 +1,368 @@
+#ifndef GVU_SUBBUFFER_MANAGER_H
+#define GVU_SUBBUFFER_MANAGER_H
+
+#include "../Cache/Objects.h"
+#include <set>
+#include <list>
+namespace gvu
+{
+
+/**
+ * @brief The SubBuffer struct
+ *
+ * A subbuffer is a section within a larger buffer that has been allocated
+ * with a specific offset so that it can hold array data. For example:
+ *
+ * For example, given a parent buffer B and we want to allocate 2
+ * sub buffers to hold two different types, T1 and T2
+ *
+ * If B only stored T1 or T2, this is how the array would look
+ * in the buffer
+ *   B: | T1 | T1 | T1 | T1 | T1 | T1 | T1 | T1 |
+ *   B: |    T2   |    T2   |    T2   |    T2   |
+ *
+ *
+ * now if we want to allocate sub buffer A to hold 3x T1 and
+ * sub buffer B to hold 2x T2. To properly allocate them
+ * so they can be looked up correctly, buffer B would need to
+ * look like this:
+ *
+ *       <---a size---> <-----b.allocatedSize()>
+ *       <---a size--->      <--b.size()------->
+ *   B: | T1 | T1 | T1 |    |    T2   |    T2   |
+ *       ^ a.offset()   ^    ^ b.offset()
+ *       |              |
+ *       |              b.allocatedOffset()
+ *       a.allocatedOffset()
+ *
+ * If B was a shader storage buffer, you can gain access
+ * to both a and b using a single binding
+ *
+ *
+ *     layout(std430, binding = 3) buffer  T1_name
+ *     {
+ *         T1 data[];
+ *     }  T1_data;
+ *     layout(std430, binding = 3) buffer  T2_name
+ *     {
+ *         T1 data[];
+ *     }  T1_data;
+ *
+ *
+ *     // these two values have to be passed into the shader
+ *     T1_startIndex = a.offset() / a.alignment();
+ *     T2_startIndex = b.offset() / b.alignment();
+ *
+ *     // and values in a and b can be accessed like so
+ *     T1_data.data[ T1_startIndex + i]
+ *     T2_data.data[ T2_startIndex + i]
+ *
+ */
+struct SubBuffer
+{
+    VkBuffer buffer() const
+    {
+        return m_handle->getBuffer();
+    }
+    /**
+     * @brief offset
+     * @return
+     *
+     * The aligned offset of the buffer. this should be used for binding
+     */
+    VkDeviceSize offset() const
+    {
+        return m_offset;
+    }
+
+    /**
+     * @brief size
+     * @return
+     *
+     * The size of the usable buffer requested by the
+     * user
+     */
+    VkDeviceSize size() const
+    {
+        return m_size;
+    }
+
+    /**
+     * @brief allocationSize
+     * @return
+     *
+     * The actual size of the subbuffer that was allocated
+     */
+    VkDeviceSize allocationSize() const
+    {
+        return m_allocationSize;
+    }
+    /**
+     * @brief allocationOffset
+     * @return
+     *
+     * The actual offset of the subbuffer
+     */
+    VkDeviceSize allocationOffset() const
+    {
+        return m_allocationOffset;
+    }
+
+    /**
+     * @brief alignment
+     * @return
+     *
+     * The alignment of the buffer.
+     *
+     * offset() % alignment() == 0
+     */
+    VkDeviceSize alignment() const
+    {
+        return m_alignment;
+    }
+
+    /**
+     * @brief shaderStorageArrayStartIndex
+     * @return
+     *
+     */
+    uint32_t shaderStorageArrayStartIndex() const
+    {
+        return offset() / alignment();
+    }
+protected:
+    BufferHandle m_handle;
+
+    // the actual buffer size and the offset
+    // from the host buffer
+    VkDeviceSize m_allocationOffset = 0;
+    VkDeviceSize m_allocationSize = 0;
+
+    // the usable location
+    VkDeviceSize m_offset    = 0;
+    VkDeviceSize m_size      = 0;
+    VkDeviceSize m_alignment = 1;
+    friend class SubBufferManager;
+};
+using SubBufferHandle   = std::shared_ptr<SubBuffer>;
+
+/**
+ * @brief The SubBufferManager class
+ *
+ * The SubBufferManager is essentially a memory pool for buffers
+ * that exist within another buffer. This works by allocating
+ * a single buffer (ie: 100M) and then allocating sections
+ * of that to use for various purposes.
+ */
+class SubBufferManager
+{
+public:
+    /**
+     * @brief setBuffer
+     * @param h
+     * @param allocationChunkSize
+     *
+     * Sets the buffer for this manager. This will essentially reset
+     * all allocations.
+     *
+     * The allocationChunkSize will ensure that all allocations are
+     * multiples of that value
+     */
+    void setBuffer(BufferHandle h, VkDeviceSize allocationChunkSize=256)
+    {
+        m_buffer = h;
+        m_allocationChunkSize = allocationChunkSize;
+        auto m_emptyMemory = std::make_shared<SubBuffer>();
+        m_emptyMemory->m_allocationOffset = 0;
+        m_emptyMemory->m_allocationSize   = h->getBufferSize();
+
+        m_emptyMemory->m_offset    = 0;
+        m_emptyMemory->m_size      = m_emptyMemory->m_allocationSize;
+        m_emptyMemory->m_alignment = 1;
+
+        m_allocations.push_back(m_emptyMemory);
+    }
+
+    /**
+     * @brief allocate
+     * @param s
+     * @param alignment
+     * @return
+     *
+     * Allocate a subbuffer from this buffer manager
+     * and return the SubBuffer handle.
+     *
+     * The returned SubBuffer size will always be
+     * a multiple of the alignment you chose.
+     *
+     * alignment ensures the the offset of the subbuffer you
+     * get will be aligned to this value
+     *
+     * auto b = M.allocate(1545, 1024);
+     * assert(b->size() >= 1545)
+     * assert(b->offset() % 1024 == 0)
+     */
+    SubBufferHandle allocate(VkDeviceSize s, VkDeviceSize alignment )
+    {
+        // bump the size requirements to be multiples of the alignment
+        //s = BufferInfo::_roundUp(s, alignment);
+        //s = s % alignment == 0 ? s : (s/alignment+1)*alignment;
+
+        assert(m_allocations.size() > 0);
+        for(auto it=std::prev(m_allocations.end()); ;)
+        {
+            auto & E = *it;
+            // use_count() == 1 means this chunk was allocate
+            // but no longer used
+            if(E.use_count() == 1)
+            {
+                // check if any of the next allocations are
+                // able to be merged into this empty allocation
+                _mergeForward(it);
+
+                auto firstByte    = E->allocationOffset();
+                auto alignedBegin = BufferInfo::_roundUp(firstByte, alignment);
+                auto alignedEnd   = BufferInfo::_roundUp(alignedBegin + s, alignment);
+                auto lastByte     = BufferInfo::_roundUp(alignedEnd,m_allocationChunkSize);
+
+                auto allocationSize = lastByte-firstByte;
+                s = alignedEnd - alignedBegin;
+
+                if(E->allocationSize() >= allocationSize)
+                {
+                    auto B = std::make_shared<SubBuffer>(*E);
+
+                    B->m_allocationSize   = allocationSize;
+                    B->m_allocationOffset = E->allocationOffset();
+                    B->m_size             = s;
+                    B->m_offset           = alignedBegin;
+                    B->m_alignment        = alignment;
+
+                    E->m_allocationOffset = B->m_allocationOffset + B->m_allocationSize;
+                    E->m_allocationSize   -= allocationSize;
+                    E->m_offset           = E->m_allocationOffset;
+                    E->m_size             = E->m_allocationSize;
+                    E->m_alignment        = 1;
+
+                    // insert the newly allocated buffer
+                    // into the allocation list right before E
+                    m_allocations.insert(it, B);
+
+                    // no need to keep E around if the allocation size is zero
+                    if(E->allocationSize() == 0)
+                    {
+                        m_allocations.erase(it);
+                    }
+                    return B;
+                }
+            }
+            if( it == m_allocations.begin())
+                break;
+            --it;
+        }
+
+
+        return {};
+    }
+
+
+    /**
+     *
+     * Allocate a buffer using a specifc type. This is
+     * mostly just a helper function to use if you know you
+     * are going to use the buffer to only hold a certain types (eg: vec3 for positions)
+     *
+     */
+    template<typename T>
+    SubBufferHandle allocateTyped(uint32_t count)
+    {
+        auto d = allocate( sizeof(T) * count, sizeof(T));
+        assert(d->offset() % sizeof(T) == 0);
+        return d;
+    }
+
+    std::list<SubBufferHandle> const & allocations() const
+    {
+        return m_allocations;
+    }
+
+    void print(VkDeviceSize chunkSize = 256)
+    {
+        assert( std::is_sorted(m_allocations.begin(), m_allocations.end(),[](auto & a, auto & b)
+        {
+            return a->allocationOffset() < b->allocationOffset();
+        }));
+        std::string _sym = "#X@";
+        std::string _emp = "_.";
+        std::string out;
+        size_t i=0;
+
+        for(auto & a : m_allocations)
+        {
+            auto s = a->allocationSize() / chunkSize;
+            out += std::string(s, a.use_count() == 1 ? _emp[i%_emp.size()] : _sym[i%_sym.size()] );
+            ++i;
+        }
+        std::cout << out << std::endl;
+    }
+
+    static SubBufferManager createTestCase(VkDeviceSize size, VkDeviceSize offset,
+                                    VkDeviceSize allocationSize, VkDeviceSize allocationOffset,
+                                    VkDeviceSize alignment)
+    {
+        auto a = std::make_shared<gvu::SubBuffer>();
+
+        a->m_size = size;
+        a->m_offset = offset;
+        a->m_allocationSize = allocationSize;
+        a->m_allocationOffset = allocationOffset;
+        a->m_alignment = alignment;
+
+        SubBufferManager M;
+        M.m_allocations.push_back(a);
+        return M;
+    }
+protected:
+    // given an iterator to a subbuffer handle
+    // attempt to merge with next allocation if
+    // the next handle is empty
+    bool _mergeForward(std::list<SubBufferHandle>::iterator a)
+    {
+        if( a == m_allocations.end())
+            return false;
+        auto b = std::next(a);
+        if( b == m_allocations.end())
+            return false;
+        auto & A = *a;
+        auto & B = *b;
+
+        // if both of the allocations are empty
+        // then we can merge them
+        if(B.use_count() == 1)
+        {
+            assert(A->allocationOffset() + A->allocationSize() == B->allocationOffset() );
+
+            if(A->allocationOffset() + A->allocationSize() == B->allocationOffset() )
+            {
+                A->m_allocationSize += B->allocationSize();
+                A->m_size            = A->m_allocationSize;
+
+                m_allocations.erase(b);
+                return _mergeForward(a);
+            }
+        }
+        return false;
+    }
+
+
+    std::list<SubBufferHandle> m_allocations;
+    VkDeviceSize m_allocationChunkSize = 256;
+    BufferHandle m_buffer;
+};
+
+
+
+}
+
+#endif
+
---
+++ test/unit-SubBufferManager.cpp
@@ -0,0 +1,257 @@
+#include<catch2/catch.hpp>
+#include <fstream>
+
+#include "unit_helpers.h"
+#include <gvu/Core/Cache/RenderPassCache.h>
+#include <gvu/Core/Managers/SubBufferManager.h>
+
+SCENARIO("Aligned allocations")
+{
+    gvu::SubBufferManager M = gvu::SubBufferManager::createTestCase(1024, 0, 1024,0,1);
+
+
+    WHEN("We allocate 15 bytes with an alignment of 32")
+    {
+        auto b = M.allocate(15, 32);
+
+        THEN("The size and offset is 0,32")
+        {
+            REQUIRE( b->offset() == 0);
+            REQUIRE( b->size()   == 32);
+        }
+        THEN("The allocated size is 32")
+        {
+            REQUIRE( b->allocationSize()   == 256);
+            REQUIRE( b->allocationOffset() == 0);
+        }
+
+        WHEN("We allocate a buffer with 24 byte alignment")
+        {
+            auto c = M.allocate(468,24);
+
+            THEN("The offset and size is a multiple of the alignment")
+            {
+                REQUIRE( c->offset() % 24 == 0);
+                REQUIRE( c->size()   % 24 == 0);
+                REQUIRE( c->size() >= 468 );
+                REQUIRE( c->alignment() == 24);
+            }
+            THEN("The allocated size is greater than the size")
+            {
+                REQUIRE( c->allocationSize() > c->size() );
+                REQUIRE( c->allocationOffset() == 256);
+            }
+        }
+    }
+}
+
+SCENARIO("Multiple allocations")
+{
+    gvu::SubBufferManager M = gvu::SubBufferManager::createTestCase(1024, 0, 1024,0,1);
+
+
+    WHEN("We allocate 15 bytes with an alignment of 32")
+    {
+        auto a = M.allocate(256, 16);
+        M.print();
+        auto b = M.allocate(256, 16);
+        M.print();
+        auto c = M.allocate(256, 16);
+        M.print();
+        auto d = M.allocate(256, 16);
+
+
+        // |  a  |  b  |  c  |  d  |
+
+        THEN("The size and offset is 0,32")
+        {
+            REQUIRE( a->offset() == 0);
+            REQUIRE( b->offset() == 256);
+            REQUIRE( c->offset() == 512);
+            REQUIRE( d->offset() == 768);
+
+            M.print();
+            WHEN("We try to allocate again we get a nullptr")
+            {
+                auto e = M.allocate(256,16);
+                REQUIRE( e.get() == nullptr);
+            }
+
+            WHEN("We release c")
+            {
+                c.reset();
+                M.print();
+                THEN("We can allocate the same size again")
+                {
+                    auto e = M.allocate(256,8);
+                    REQUIRE(e->offset() == 512);
+                }
+            }
+        }
+    }
+}
+
+SCENARIO("Allocting multiple")
+{
+    // allocate 200Mb of mesh data
+    gvu::SubBufferManager M = gvu::SubBufferManager::createTestCase(200*1024*1024, 0,
+                                                                    200*1024*1024,0,1);
+
+
+    std::vector<VkDeviceSize> alignments = {12,16,4,8};
+    std::vector<gvu::SubBufferHandle> allocs;
+    while(true)
+    {
+        auto b = M.allocate( (rand()%5+1) * 1024*1024, alignments[rand()%4]);
+        allocs.push_back(b);
+        if(!b)
+            break;
+    }
+
+    std::cout << "Total Allocations: " << allocs.size() << std::endl;
+}
+
+SCENARIO("Allocting typed buffers")
+{
+    // allocate 200Mb of mesh data
+    gvu::SubBufferManager M = gvu::SubBufferManager::createTestCase(512*1024, 0,
+                                                                    512*1024,0,1);
+
+
+    auto b = M.allocateTyped<float>(300);
+    {
+        REQUIRE(b->size() >= 300 * sizeof(float));
+        REQUIRE(b->offset() % sizeof(float) == 0);
+        REQUIRE(b->allocationSize() % 256 == 0);
+        REQUIRE(b->allocationOffset() % 256 == 0);
+    }
+
+    auto c = M.allocateTyped< std::array<float, 3> >(500);
+    {
+        REQUIRE(c->size() >= 500 * sizeof(float));
+        REQUIRE(c->offset() % sizeof(float) == 0);
+        REQUIRE(c->alignment() == sizeof( std::array<float,3>));
+        REQUIRE(c->allocationSize() % 256 == 0);
+        REQUIRE(c->allocationOffset() % 256 == 0);
+    }
+
+
+    auto d = M.allocateTyped< std::array<float, 3> >(500);
+    c.reset();
+    {
+        REQUIRE(d->size() >= 500 * sizeof(float));
+        REQUIRE(d->offset() % sizeof(float) == 0);
+        REQUIRE(d->alignment() == sizeof( std::array<float,3>));
+        REQUIRE(d->allocationSize() % 256 == 0);
+        REQUIRE(d->allocationOffset() % 256 == 0);
+    }
+    // bind the vertex buffers like so:
+    // VkBuffer     buffers[] = { b->buffer(), c->buffer()};
+    // VkDeviceSize offsets[] = { b->offset()   , c->offset()};
+    // vkCmdBindVertexBuffers({}, 0, 2, buffers, offsets);
+
+    // we want to be able to do something like this:
+
+
+
+    M.print();
+}
+
+
+SCENARIO("Test merge allocation")
+{
+    GIVEN("A manager with 5 allocations that span the entire buffer")
+    {
+        // allocate 200Mb of mesh data
+        gvu::SubBufferManager M = gvu::SubBufferManager::createTestCase(5*1024, 0,
+                                                                        5*1024,0,1);
+
+
+        auto a = M.allocate(1024, 4);
+        auto b = M.allocate(1024, 4);
+        auto c = M.allocate(1024, 4);
+        auto d = M.allocate(1024, 4);
+        auto e = M.allocate(1024, 4);
+        M.print();
+
+        REQUIRE(M.allocations().size() == 5);
+
+        WHEN("We reset the 3 middle allocations")
+        {
+            d.reset();
+            c.reset();
+            b.reset();
+            M.print();
+
+            THEN("When we allocate 1024*2")
+            {
+                auto f = M.allocate(2048, 4);
+
+                REQUIRE(M.allocations().size() == 4);
+                REQUIRE( f->allocationOffset() == 1024*2);
+                M.print();
+
+            }
+            THEN("When we allocate 1024*3")
+            {
+                auto g = M.allocate(1024*3, 4);
+                REQUIRE(M.allocations().size() == 3);
+                REQUIRE( g->allocationOffset() == 1024);
+                M.print();
+            }
+
+        }
+    }
+}
+
+#if 0
+SCENARIO( " Scenario 1: Create a Sampler" )
+{
+    // create a default window and initialize all vulkan
+    // objects.
+    auto window = createWindow(1024,768);
+
+    // resize the framegraph to the size of the
+    // swapchain. This will allocate any internal
+    // images which depend on the size of the swapchain (eg: gBuffers)
+    auto e = window->getSwapchainExtent();
+    (void)e;
+
+    using CacheType = gvu::RenderPassCache;
+
+    CacheType cache;
+    cache.init(window->getDevice());
+
+    // Test creating the same object twice
+    //--------------------------------------------------------------
+    CacheType::createInfo_type ci = CacheType::createInfo_type::createSimpleRenderPass( {{VK_FORMAT_R8G8B8A8_UNORM, VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL}});
+    //--------------------------------------------------------------
+
+    auto obj1 = cache.create(ci);
+    auto obj2 = cache.create(ci);
+
+    REQUIRE(obj1 == obj2);
+    //--------------------------------------------------------------
+
+
+    // Test creating a different object
+    //--------------------------------------------------------------
+    CacheType::createInfo_type ci2 = CacheType::createInfo_type::createSimpleRenderPass( {{VK_FORMAT_R8G8B8A8_UNORM, VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL}},
+                                                                                        {VK_FORMAT_D32_SFLOAT, VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL});
+    auto obj3 = cache.create(ci2);
+    REQUIRE(obj1 != obj3);
+    //--------------------------------------------------------------
+
+    // Destroy the cache
+    cache.destroy(); B->m_alloca
+
+    window->destroy();
+    window.reset();
+
+    SDL_Quit();
+
+}
+
+#endif
+
+
