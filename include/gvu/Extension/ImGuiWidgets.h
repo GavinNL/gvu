ca66277c726dfb8b97015c2037f227101512dd0d
Gavin <gavin.n.lobo@gmail.com>
2022-10-21 17:28:01 -0400

    Added imguiwidget helpers

---
+++ include/gvu/Extension/ImGuiWidgets.h
@@ -0,0 +1,58 @@
+#ifndef GVU_IMGUI_WIDGETS_H
+#define GVU_IMGUI_WIDGETS_H
+
+#include "../Core/Managers/SubBufferManager.h"
+#include <imgui.h>
+
+namespace gul
+{
+void drawAllocation(gvu::SubBufferManager & M)
+{
+
+    auto & A = M.allocations();
+    ImGui::Begin("Allocations");
+
+    if(ImGui::Button("Free"))
+    {
+        M.mergeFreeAllocations();
+    }
+    ImGui::PushStyleVar(ImGuiStyleVar_ItemSpacing, {0,0});
+
+    const uint32_t allocatedColors[] = {0xFF999999,0xFFBBBBBB,0xFFDDDDDD};
+    const uint32_t freeColors[] = {0xFF222222,0xFF333333};
+    uint32_t i=0;
+    uint32_t j=0;
+
+    for(auto &a : A)
+    {
+        auto remainingSpace = ImGui::GetContentRegionAvail().x;
+        auto s = a->allocationSize() / 1024;
+        ImGui::PushStyleColor(ImGuiCol_Button, a.use_count() == 1 ? freeColors[(++j)%2]: allocatedColors[(++i)%3]);
+
+        ImGui::PushID(&a);
+        while(remainingSpace < s)
+        {
+            ImGui::Button("", { remainingSpace, 0});
+            if(ImGui::IsItemHovered())
+            {
+                ImGui::SetTooltip("Size: %lu  Use Count: %lu", a->allocationSize(), a.use_count());
+            }
+            s -= remainingSpace;
+            remainingSpace = ImGui::GetContentRegionAvail().x;
+        }
+        ImGui::Button("", { static_cast<float>(s), 0});
+        if(ImGui::IsItemHovered())
+        {
+            ImGui::SetTooltip("Size: %lu  Use Count: %lu", a->allocationSize(), a.use_count());
+        }
+        ImGui::PopID();
+        ImGui::PopStyleColor(1);
+        ImGui::SameLine();
+    }
+    ImGui::PopStyleVar(1);
+    ImGui::End();
+}
+
+}
+
+#endif
